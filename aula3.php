c1bb33f9e576032d62e70e4570fffed933e0d920
batistaanacletocarolinaana <165102758+batistaanacletocarolinaana@users.noreply.github.com>
2024-06-20 19:28:25 -0300

    Add files via upload

---
+++ aula3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html>
+    <body>
+        <?php
+        $idade = 21;
+        $idantidade = true ;
+        
+        if ($idade > 18 && $identidade == true ){
+          echo 'seja bem-vindo!';
+        }
+        ?>
+        <?php
+           $idade = 17 ;
+           if($idade < 18) {
+               echo ' voçê não pode entra aqui!';
+           } else {
+               echo ' seja bem-vindo';
+           }
+        ?>
+    </body>
+</html>
